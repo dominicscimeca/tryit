a4a63c51194bcec80854038a2d8b912530d9bb53
dscimeca <dscimeca@cj.com>
2018-08-23 23:14:31 -0700

    adding sinon chai

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,9 @@ import chai from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinonChai from "sinon-chai";
 
 Enzyme.configure({ adapter: new Adapter() });
-
+chai.should();
 chai.use(chaiEnzyme());
+chai.use(sinonChai);
