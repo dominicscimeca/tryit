7e8b9182689509bda726c45b8abe284996dcd40f
dscimeca <dscimeca@cj.com>
2018-08-23 22:39:33 -0700

    adding products to app

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './Header'
+import Products from './Products'
 import chai from 'chai'
 const expect = chai.expect;
 import { shallow } from 'enzyme';
@@ -16,5 +17,10 @@ describe('App', function () {
         const app = shallow(<App />);
         const header = app.find(Header);
         expect(header).to.have.length(1);
+    });
+    it('should include Products', function () {
+        const app = shallow(<App />);
+        const products = app.find(Products);
+        expect(products).to.have.length(1);
     });
 });
